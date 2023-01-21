7dacb23357102958a8ed040416c6669563d4b6bd
Lucas Epherra <letoso13@gmail.com>
2023-01-21 12:52:36 -0300

    ItemCount fixed

--- src/componentes/ItemCount/ItemCount.js
+++ src/componentes/ItemCount/ItemCount.js
@@ -1,38 +1,30 @@
-import { useState } from 'react';
-import './itemCount.css';
+import { useState } from "react";
+import "./itemCount.css";
 
 const ItemCount = () => {
-
-  const [counter, setCounter] = useState(0)
-
-  const sumar = () => {
-
-    setCounter(counter + 1)
-  }
+  const [cantidad, setCantidad] = useState(1);
 
   const restar = () => {
+    cantidad > 1 && setCantidad(cantidad - 1);
+  };
 
-    setCounter(counter - 1)
-  }
+  const sumar = () => {
+    cantidad < 20 && setCantidad(cantidad + 1);
+  };
 
   return (
-
-    <div className='container countCard'>
-
+    <div className="container countCard">
       <strong>
-        <p>
-          Cantidad: 
-        </p>
+        <p>Cantidad:</p>
       </strong>
 
-    <section className='clicker mx-2'>
-      <button className='btn btn-secondary mx-2' onClick={sumar}>+</button>
-      <p className='count'>{counter}</p>
-      <button className='btn btn-secondary mx-2' onClick={restar}>-</button>
-    </section>
-
+      <section className="clicker mx-2">
+        <button className="btn btn-secondary mx-2" onClick={restar}> - </button>
+        <span className="count">{cantidad}</span>
+        <button className="btn btn-secondary mx-2" onClick={sumar}> + </button>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default ItemCount;
