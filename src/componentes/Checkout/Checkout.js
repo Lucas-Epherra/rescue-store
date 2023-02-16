c31d9cc8eae3de6050043d58d72a665aaca80dff
Lucas Epherra <letoso13@gmail.com>
2023-02-16 19:42:27 -0300

    flujo de datos finalizado , faltan algunos controles estrictos

--- src/componentes/Checkout/Checkout.js
+++ src/componentes/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import { CartContext } from "../../context/CartContext";
 import { Link, Navigate } from "react-router-dom";
 import "./Checkout.css";
 import { db } from "../../firebase/config";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, updateDoc, getDoc, doc } from "firebase/firestore";
 
 const Checkout = () => {
   const { cart, sumarTotalCart, vaciarCart } = useContext(CartContext);
@@ -64,6 +64,19 @@ const Checkout = () => {
     };
 
     const ordersRef = collection(db, "orders");
+    const productosRef = collection(db, "productos");
+    cart.forEach((prod) => {
+      const docRef = doc(productosRef, prod.id);
+      getDoc(docRef).then((doc) => {
+        if (doc.data().stock - prod.cantidad >= 0) {
+          updateDoc(docRef, { stock: doc.data().stock - prod.cantidad });
+        } else {
+          alert("No hay mas stock");
+        }
+      });
+   
+    });
+
     addDoc(ordersRef, orden)
       .then((doc) => {
         setOrderId(doc.id);
@@ -72,76 +85,84 @@ const Checkout = () => {
       .catch((error) => console.log(error));
   };
 
-  if (orderId){
-      return(
-        <div className="container my-5 finCompra">
-          <h2>Tu compra ha sido exitosa</h2>
-          <hr/>
-          <p>Muchas gracias por tu compra</p>
-          <p><strong>Tu codigo de orden es :</strong> {orderId}</p>
-          <Link to="/" className="btn btn-success">Volver al inicio</Link>
-        </div>
-      )
+  if (orderId) {
+    return (
+      <div className="container my-5 finCompra">
+        <h2>Tu compra ha sido exitosa</h2>
+        <hr />
+        <p>Muchas gracias por tu compra</p>
+        <p>
+          <strong>Tu codigo de orden es :</strong> {orderId}
+        </p>
+        <Link to="/" className="btn btn-success">
+          Volver al inicio
+        </Link>
+      </div>
+    );
   }
 
-  if (error.direccion === false || error.email === false || error.nombre === false){
-    return(
+  if (
+    error.direccion === false ||
+    error.email === false ||
+    error.nombre === false
+  ) {
+    return (
       <div className="container my-5">
-      <h2>Terminar mi compra</h2>
-      <hr />
-      <form onSubmit={handleSumbit}>
-        <label>Nombre :</label>
-        <input
-          className="form-control my-2"
-          onChange={handleInputChange}
-          type="text"
-          name="nombre"
-          values={values.nombre}
-          placeholder="Tu nombre"
-        />
-        {error.nombre === false ? (
-          <p className="errorMsj">Ha ocurrido un error, nombre invalido</p>
-        ) : (
-          ""
-        )}
+        <h2>Terminar mi compra</h2>
+        <hr />
+        <form onSubmit={handleSumbit}>
+          <label>Nombre :</label>
+          <input
+            className="form-control my-2"
+            onChange={handleInputChange}
+            type="text"
+            name="nombre"
+            values={values.nombre}
+            placeholder="Tu nombre"
+          />
+          {error.nombre === false ? (
+            <p className="errorMsj">Ha ocurrido un error, nombre invalido</p>
+          ) : (
+            ""
+          )}
 
-        <label>Direccion :</label>
-        <input
-          className="form-control my-2"
-          onChange={handleInputChange}
-          type="text"
-          name="direccion"
-          values={values.direccion}
-          placeholder="Tu direccion"
-        />
-        {error.direccion === false ? (
-          <p className="errorMsj">Ha ocurrido un error, direccion invalida</p>
-        ) : (
-          ""
-        )}
+          <label>Direccion :</label>
+          <input
+            className="form-control my-2"
+            onChange={handleInputChange}
+            type="text"
+            name="direccion"
+            values={values.direccion}
+            placeholder="Tu direccion"
+          />
+          {error.direccion === false ? (
+            <p className="errorMsj">Ha ocurrido un error, direccion invalida</p>
+          ) : (
+            ""
+          )}
 
-        <label>Email :</label>
-        <input
-          className="form-control my-2"
-          onChange={handleInputChange}
-          type="email"
-          name="email"
-          values={values.email}
-          placeholder="Tu email"
-        />
-        {error.email === false ? (
-          <p className="errorMsj">Ha ocurrido un error, email invalido</p>
-        ) : (
-          ""
-        )}
+          <label>Email :</label>
+          <input
+            className="form-control my-2"
+            onChange={handleInputChange}
+            type="email"
+            name="email"
+            values={values.email}
+            placeholder="Tu email"
+          />
+          {error.email === false ? (
+            <p className="errorMsj">Ha ocurrido un error, email invalido</p>
+          ) : (
+            ""
+          )}
 
-        <button className="btn btn-primary mx-3 my-3">Enviar</button>
-      </form>
-      <Link to="/cart" className="btn btn-danger">
-        Volver al carrito
-      </Link>
-    </div>
-    )
+          <button className="btn btn-primary mx-3 my-3">Enviar</button>
+        </form>
+        <Link to="/cart" className="btn btn-danger">
+          Volver al carrito
+        </Link>
+      </div>
+    );
   }
 
   if (cart.length === 0) {
@@ -162,7 +183,7 @@ const Checkout = () => {
           values={values.nombre}
           placeholder="Tu nombre"
         />
-        
+
         <label>Direccion :</label>
         <input
           className="form-control my-2"
@@ -172,7 +193,7 @@ const Checkout = () => {
           values={values.direccion}
           placeholder="Tu direccion"
         />
-      
+
         <label>Email :</label>
         <input
           className="form-control my-2"
@@ -182,7 +203,7 @@ const Checkout = () => {
           values={values.email}
           placeholder="Tu email"
         />
-      
+
         <button className="btn btn-primary mx-3 my-3">Enviar</button>
       </form>
       <Link to="/cart" className="btn btn-danger">
