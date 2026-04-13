a586577627d061f2f2929be07f9785866c59868e
Lucas Epherra <letoso13@gmail.com>
2026-04-13 19:25:47 -0300

    edit cart-widget

--- src/componentes/CartWidget/CartWidget.js
+++ src/componentes/CartWidget/CartWidget.js
@@ -1,27 +1,26 @@
-import './cartWidget.css';
-import cart from '../../assets/cart.svg'
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { CartContext } from '../../context/CartContext';
-
-
+import "./cartWidget.css";
+import cart from "../../assets/cart.svg";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 function CartWidget() {
+  const { itemsTotales } = useContext(CartContext);
+  const totalItems = itemsTotales();
 
-    const { itemsTotales } = useContext(CartContext)
+  return (
+    <Link
+      to="/cart"
+      className="cartLink"
+      aria-label={`Carrito con ${totalItems} productos`}
+    >
+      <span className="cartIconBox">
+        <img src={cart} className="cartIcon" alt="Carrito" />
+      </span>
 
-    return (
-        <Link to="/cart" className="d-flex cart">
-            <img
-                src={cart}
-                width="30"
-                height="30"
-                className="align-center"
-                alt="cart logo"
-            />
-            <p className="m-1 numProductos">{itemsTotales()}</p>
-        </Link>
-    );
+      {totalItems > 0 && <span className="numProductos">{totalItems}</span>}
+    </Link>
+  );
 }
 
 export default CartWidget;
